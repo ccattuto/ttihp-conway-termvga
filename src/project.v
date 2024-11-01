ad6d9dcde9332436ab763b54bee7d41a902cd268
Ciro Cattuto <ciro.cattuto@gmail.com>
2024-11-01 10:26:19 +0100

    Initial checkin

--- src/project.v
+++ src/project.v
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2024 Ciro Cattuto <ciro.cattuto@gmail.com>
  * SPDX-License-Identifier: Apache-2.0
  */
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_ccattuto_conway (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,12 +16,531 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+// -------------- I/O PINS ---------------------------
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+// All output pins must be assigned. If not used, assign to 0.
+assign uo_out[3:0] = 0;
+assign uo_out[7:5] = 0;
+assign uio_oe  = 8'b1;
+
+// UART signals
+wire uart_rx, uart_tx;
+assign uart_rx = ui_in[3];
+assign uo_out[4] = uart_tx;
+
+// clock
+localparam CLOCK_FREQ = 24000000;
+
+// reset
+wire boot_reset;
+assign boot_reset = ~rst_n;
+
+// TinyVGA PMOD
+assign uio_out = {hsync, B[0], G[0], R[0], vsync, B[1], G[1], R[1]};
+
+
+// -------------- UART TRANSMITTER ---------------------------
+
+wire uart_tx_en;
+assign uart_tx_en = 1;
+
+reg [7:0]   uart_tx_data;
+reg         uart_tx_valid;
+wire        uart_tx_ready;
+
+UARTTransmitter #(
+    .CLOCK_RATE(CLOCK_FREQ),
+    .BAUD_RATE(115200)
+) uart_tx_inst (
+    .clk(clk),
+    .reset(boot_reset),       // reset
+    .enable(uart_tx_en),      // TX enable
+    .valid(uart_tx_valid),    // start of TX
+    .in(uart_tx_data),        // data to transmit
+    .out(uart_tx),            // TX signal
+    .ready(uart_tx_ready)     // read for TX data
+);
+
+
+// -------------- UART RECEIVER ---------------------------
+
+wire uart_rx_en;
+assign uart_rx_en = 1;
+
+wire [7:0]  uart_rx_data;
+wire        uart_rx_valid;
+wire        uart_rx_error;
+wire        uart_rx_overrun;
+reg         uart_rx_ready;
+
+UARTReceiver #(
+    .CLOCK_RATE(CLOCK_FREQ),
+    .BAUD_RATE(115200)
+) uart_rx_inst (
+    .clk(clk),
+    .reset(boot_reset),       // reset
+    .enable(uart_rx_en),      // RX enable
+    .in(uart_rx),             // RX signal
+    .ready(uart_rx_ready),    // ready to consume RX data
+    .out(uart_rx_data),       // RX wires
+    .valid(uart_rx_valid),    // RX completed
+    .error(uart_rx_error),    // RX error
+    .overrun(uart_rx_overrun) // RX overrun
+);
+
+
+// ---------------- VGA ---------------
+
+wire hsync;
+wire vsync;
+wire [1:0] R;
+wire [1:0] G;
+wire [1:0] B;
+wire video_active;
+wire [9:0] pix_x;
+wire [9:0] pix_y;
+
+hvsync_generator hvsync_inst (
+  .clk(clk),
+  .reset(boot_reset),
+  .hsync(hsync),
+  .vsync(vsync),
+  .display_on(video_active),
+  .hpos(pix_x),
+  .vpos(pix_y)
+);
+
+wire frame_active;
+assign frame_active = (pix_x >= 80 && pix_x < 640-80) ? 1 : 0;
+
+wire [2:0] cell_x_index;
+wire [2:0] cell_y_index;
+
+assign cell_x_index =
+  (pix_x < 140) ? 0 :
+  (pix_x < 200) ? 1 :
+  (pix_x < 260) ? 2 :
+  (pix_x < 320) ? 3 :
+  (pix_x < 380) ? 4 :
+  (pix_x < 440) ? 5 :
+  (pix_x < 500) ? 6 :
+  (pix_x < 560) ? 7 : 0;
+
+assign cell_y_index =
+  (pix_y < 60) ? 0 :
+  (pix_y < 120) ? 1 :
+  (pix_y < 180) ? 2 :
+  (pix_y < 240) ? 3 :
+  (pix_y < 300) ? 4 :
+  (pix_y < 360) ? 5 :
+  (pix_y < 420) ? 6 : 7;
+
+wire [5:0] cell_index;
+assign cell_index = (cell_y_index << 3) | cell_x_index;
+
+// grid lines
+wire [5:0] grid_x;
+wire [5:0] grid_y;
+wire grid;
+assign grid_x = pix_x - 80 - (cell_x_index << 6) + (cell_x_index << 2);
+assign grid_y = pix_y - (cell_y_index << 6) + (cell_y_index << 2);
+assign grid = (grid_x < 2 || grid_x > 57 || grid_y < 2 || grid_y > 57) ? 0 : 1;
+
+assign R = (video_active & frame_active) ? {board_state[cell_index] & grid, 1'b0} : 2'b00;
+assign G = (video_active & frame_active) ? {board_state[cell_index] & grid, 1'b1} : 2'b00;
+assign B = {1'b0, video_active & frame_active & grid};
+
+
+// ----------------- RNG ----------------------
+
+wire rng;
+
+lfsr_rng lfsr(
+  .clk(clk),
+  .reset(boot_reset),
+  .random_bit(rng)
+);
+
+
+// ----------------- SIMULATION PARAMS -------------------------
+
+localparam logWIDTH = 3, logHEIGHT = 3;         // 8x8 board
+localparam UPDATE_INTERVAL = CLOCK_FREQ / 5;    // 5 Hz simulation update
+
+localparam WIDTH = 2 ** logWIDTH;
+localparam HEIGHT = 2 ** logHEIGHT;
+localparam BOARD_SIZE = WIDTH * HEIGHT;
+
+reg board_state [0:BOARD_SIZE-1];         // current state of the simulation
+reg [BOARD_SIZE-1:0] board_state_next;    // next state of the simulation
+
+
+// ----------------- SIMULATION CONTROL VIA UART RX --------------------
+
+localparam ACTION_IDLE = 0, ACTION_UPDATE = 1, ACTION_COPY = 2, ACTION_DISPLAY = 3, ACTION_RND = 4, ACTION_INIT = 5, ACTION_LOAD_INIT = 6, ACTION_DISPLAY_INIT = 7;
+reg [2:0] action;
+
+reg running;  // high when simulation is advancing automatically based on timer
+reg [31:0] timer;
+
+reg [logWIDTH+logHEIGHT-1:0] index;    // index of cell being updated
+wire [logWIDTH-1:0] cell_x;             // x-coordinate (column) of cell being updated
+wire [logHEIGHT-1:0] cell_y;            // y coordinate (row) of cell being updated
+assign cell_x = index[logWIDTH-1:0];
+assign cell_y = index[logWIDTH+logHEIGHT-1:logWIDTH];
+
+reg [3:0] neigh_index;                  // index of neighboring cell (0 to 7)
+reg [3:0] num_neighbors;                // number of neighbors of current cell
+
+localparam HEIGHT_MASK = {logHEIGHT{1'b1}};
+localparam WIDTH_MASK = {logWIDTH{1'b1}};
+
+localparam TX_IDLE = 0, TX_SEND = 1, TX_WAIT = 2, TX_SEND_CRLF = 3, TX_WAIT_CRLF = 4, TX_SEND_HOME = 5, TX_WAIT_HOME = 6, TX_INIT = 7, TX_WAIT_INIT = 8;
+reg [3:0] txstate;
+reg [logWIDTH:0] colindex;            // column index of current cell
+reg [5:0] txindex;                    // index into strings to be printed (welcome message, formatting)
+
+localparam CELL_ALIVE_CHAR = 79;      // "O" is used to display alive cells
+localparam CELL_DEAD_CHAR = 32;       // " " is used to display dead cells
+
+
+// STATE TRANSITION LOGIC
+//
+// - ACTION_UPDATE computes board_state_next based on board_state
+// - ACTION_COPY copies board_state_next over board_state
+// - ACTION_DISPLAY prints out board_state over UART (ANSI terminal)
+// - ACTION_RND randomizes board_state
+
+always @(posedge clk) begin
+  if (boot_reset) begin
+    action <= ACTION_INIT;
+    running <= 0;
+    timer <= 0;
+
+    uart_rx_ready <= 0;
+    uart_tx_data <= 0;
+    uart_tx_valid <= 0;
+
+    index <= 0;
+    neigh_index <= 0;
+    num_neighbors <= 0;
+
+    txstate <= TX_INIT;
+    colindex <= 0;
+    txindex <= 0;
+  end else begin
+    case (action)
+      // at init, any received character triggers randomized initialization and refresh over UART
+      ACTION_INIT: begin
+        if (!(uart_rx_valid & uart_rx_ready)) begin
+          uart_rx_ready <= 1;
+        end else begin
+          action <= ACTION_LOAD_INIT;
+          uart_rx_ready <= 0;
+        end
+      end
+
+      // idle loop: simulation controlled via characters received over UART 
+      ACTION_IDLE: begin
+        if (uart_rx_valid & uart_rx_ready) begin
+          uart_rx_ready <= 0;
+          
+          case (uart_rx_data)
+            // '0' randomizes the state of the simulation
+            48: begin
+              action <= ACTION_RND;
+            end
+
+            // '1' advances the simulation by 1 step
+            49: begin
+              if (~running) begin
+                action <= ACTION_UPDATE;
+              end else begin
+                running <= 0;
+                timer <= 0;
+              end
+            end
+
+            // ' ' toggles simulation timer-based advancing
+            32: begin
+              running <= ~running;
+              timer <= 0;
+            end
+
+            default: begin
+              action <= ACTION_IDLE;
+            end
+          endcase
+        end else if (uart_rx_valid) begin
+          uart_rx_ready <= 1;
+        end else if (running) begin // timer-based update trigger
+          if (timer < UPDATE_INTERVAL) begin
+            timer <= timer + 1;
+          end else begin
+            timer <= 0;
+            uart_rx_ready <= 0;
+            action <= ACTION_UPDATE;
+          end
+        end
+      end
+
+      // ----------------- ACTION: RANDOMIZE SIMULATION STATE --------------------
+
+      ACTION_RND: begin
+        board_state[index] <= rng;
+        if (index < BOARD_SIZE - 1) begin
+          index <= index + 1;
+        end else  begin
+          index <= 0;
+          txstate <= TX_SEND_HOME;
+          action <= ACTION_DISPLAY;
+        end
+      end
+
+      // ----------------- ACTION: LOAD (LONG) PERIODIC PATTERN --------------------
+      // (found using https://github.com/urish/tt05-silife-max/tree/main/utils)
+
+      ACTION_LOAD_INIT: begin
+        board_state[index] <= board_state_init[index];
+        if (index < BOARD_SIZE - 1) begin
+          index <= index + 1;
+        end else  begin
+          index <= 0;
+          action <= ACTION_DISPLAY_INIT;
+        end
+      end
+
+      // ----------------- ACTION: COMPUTE SIMULATION'S NEXT STATE --------------------
+
+      ACTION_UPDATE: begin
+        // loop over the 8 neighbors of the current cell
+        case (neigh_index)
+          0: begin // (-1, +1)
+            num_neighbors <= num_neighbors + board_state[((cell_y + 1) & HEIGHT_MASK) << logWIDTH | ((cell_x - 1) & WIDTH_MASK)];
+            neigh_index <= neigh_index + 1;
+          end
+
+          1: begin // (0, +1)
+            num_neighbors <= num_neighbors + board_state[((cell_y + 1) & HEIGHT_MASK) << logWIDTH | ((cell_x + 0) & WIDTH_MASK)];
+            neigh_index <= neigh_index + 1; 
+          end
+
+          2: begin // (+1, +1)
+            num_neighbors <= num_neighbors + board_state[((cell_y + 1) & HEIGHT_MASK) << logWIDTH | ((cell_x + 1) & WIDTH_MASK)];
+            neigh_index <= neigh_index + 1;
+          end
+
+          3: begin // (-1, 0)
+            num_neighbors <= num_neighbors + board_state[((cell_y + 0) & HEIGHT_MASK) << logWIDTH | ((cell_x - 1) & WIDTH_MASK)];
+            neigh_index <= neigh_index + 1;
+          end
+
+          4: begin // (+1, 0)
+            num_neighbors <= num_neighbors + board_state[((cell_y + 0) & HEIGHT_MASK) << logWIDTH | ((cell_x + 1) & WIDTH_MASK)];
+            neigh_index <= neigh_index + 1;
+          end
+
+          5: begin // (-1, -1)
+            num_neighbors <= num_neighbors + board_state[((cell_y - 1) & HEIGHT_MASK) << logWIDTH | ((cell_x - 1) & WIDTH_MASK)];
+            neigh_index <= neigh_index + 1;
+          end
+
+          6: begin // (0, -1)
+            num_neighbors <= num_neighbors + board_state[((cell_y - 1) & HEIGHT_MASK) << logWIDTH | ((cell_x + 0) & WIDTH_MASK)];
+            neigh_index <= neigh_index + 1;
+          end
+
+          7: begin // (+1, -1)
+            num_neighbors <= num_neighbors + board_state[((cell_y - 1) & HEIGHT_MASK) << logWIDTH | ((cell_x + 1) & WIDTH_MASK)];
+            neigh_index <= neigh_index + 1;
+          end
+
+          // this state (neigh_index = 8) is used to compute the new state of the current cell
+          // according to the rules of Conway's Game of Life
+          8: begin
+            board_state_next <= { board_state_next[BOARD_SIZE-2:0], (board_state[index] && (num_neighbors == 2)) | (num_neighbors == 3) };
+            //board_state_next[index] <= (board_state[index] && (num_neighbors == 2)) | (num_neighbors == 3);
+
+            neigh_index <= 0;
+            num_neighbors <= 0;
+
+            // advance to next cell to be updated, or terminate
+            if (index < BOARD_SIZE - 1) begin
+              index <= index + 1;
+            end else begin
+              index <= 0;
+              action <= ACTION_COPY;
+            end
+          end
+
+          default: begin
+            neigh_index <= 0;
+          end
+        endcase
+      end
+
+      // --------------- ACTION: COPY NEW SIMULATION STATE OVER OLD ONE --------------------
+
+      ACTION_COPY: begin
+        if (vsync) begin // synchronize to vsync
+          // board_state[index] <= board_state_next[index];
+          board_state[index] <= board_state_next[BOARD_SIZE-1];
+          board_state_next <= {board_state_next[BOARD_SIZE-2:0], 1'b0};
+          if (index < BOARD_SIZE - 1) begin
+            index <= index + 1;
+          end else begin
+            index <= 0;
+            action <= ACTION_DISPLAY;
+            txstate <= TX_SEND_HOME;
+          end
+        end
+      end
+
+      // --------------- ACTION: DISPLAY SIMULATION STATE OVER UART --------------------
+
+      ACTION_DISPLAY: begin
+        case (txstate)
+          // Sends ASCII character corresponding to current cell state,
+          // then waits (TX_WAIT) for UART transmitted to be ready
+          // and advances to the next cell.
+          TX_SEND: begin
+            if (colindex < WIDTH) begin
+              uart_tx_data <= board_state[index] ? CELL_ALIVE_CHAR : CELL_DEAD_CHAR; // cell state to ASCII char
+              index <= index + 1;
+              colindex <= colindex + 1;
+              txstate <= TX_WAIT;
+            end else begin
+              colindex <= 0;
+              txstate <= TX_SEND_CRLF;
+            end
+          end
+
+          TX_WAIT: begin
+            if (uart_tx_ready && !uart_tx_valid ) begin
+              uart_tx_valid <= 1;
+            end else if (uart_tx_valid && !uart_tx_ready) begin
+              uart_tx_valid <= 0;
+              if (index != 0) begin
+                txstate <= TX_SEND;
+              end else begin
+                txstate <= TX_IDLE;      
+                action <= ACTION_IDLE;
+              end
+            end
+          end        
+
+          // Sends CR+LF (used at the end of every row)
+          TX_SEND_CRLF: begin
+            if (txindex < 2) begin
+              uart_tx_data <= (txindex == 0) ? 13 : 10;
+              txindex <= txindex + 1;
+              txstate <= TX_WAIT_CRLF;
+            end else begin
+              txindex <= 0;
+              txstate <= TX_SEND;              
+            end
+          end
+
+          TX_WAIT_CRLF: begin
+            if (uart_tx_ready && !uart_tx_valid ) begin
+              uart_tx_valid <= 1;
+            end else if (uart_tx_valid && !uart_tx_ready) begin
+              uart_tx_valid <= 0;
+              txstate <= TX_SEND_CRLF;
+            end
+          end        
+
+          // Sends ANSI "home" escape sequence [0x1b, '[', ';', 'H']
+          // moving the cursor to the top left of the terminal (0,0) 
+          TX_SEND_HOME: begin
+            if (txindex < 4) begin
+              case (txindex)
+                0: begin
+                  uart_tx_data <= 27;
+                end
+                1: begin
+                  uart_tx_data <= 91;
+                end
+                2: begin
+                  uart_tx_data <= 59;
+                end
+                3: begin
+                  uart_tx_data <= 72;
+                end
+                default:
+                  uart_tx_data <= 0;
+              endcase
+              txindex <= txindex + 1;
+              txstate <= TX_WAIT_HOME;
+            end else begin
+              txindex <= 0;
+              colindex <= 0;
+              txstate <= TX_SEND;
+            end
+          end
+
+          TX_WAIT_HOME: begin
+            if (uart_tx_ready && !uart_tx_valid ) begin
+              uart_tx_valid <= 1;
+            end else if (uart_tx_valid && !uart_tx_ready) begin
+              uart_tx_valid <= 0;
+              txstate <= TX_SEND_HOME;
+            end
+          end
+
+          default: begin
+          end
+        endcase
+      end
+
+      ACTION_DISPLAY_INIT: begin
+        case (txstate)
+          // prints out welcome message and usage instructions stored in string_init (ROM below)
+          TX_INIT: begin
+            if (txindex < STRING_INIT_LEN) begin
+              uart_tx_data <= string_init[txindex];
+              txindex <= txindex + 1;
+              txstate <= TX_WAIT_INIT;    
+            end else begin
+              txstate <= TX_IDLE;
+              txindex <= 0;
+              action <= ACTION_IDLE;
+            end
+          end
+
+          TX_WAIT_INIT: begin
+            if (uart_tx_ready && !uart_tx_valid ) begin
+              uart_tx_valid <= 1;
+            end else if (uart_tx_valid && !uart_tx_ready) begin
+              uart_tx_valid <= 0;
+              txstate <= TX_INIT;
+            end
+          end
+
+          default: begin
+          end
+        endcase
+      end
+
+      default: begin
+        action <= ACTION_IDLE;
+      end
+    endcase
+  end
+end 
+
+
+// ROM containing the welcome message with usage instructions
+localparam STRING_INIT_LEN = 57;
+reg [7:0] string_init [0:STRING_INIT_LEN-1];
+initial begin
+  $readmemh("string_init.hex", string_init);
+end
+
+// initial periodic pattern
+reg [63:0] board_state_init;
+initial begin
+  board_state_init = 64'b1100000111010000100010100100011001111110100110011101110110010111;
+end
 
 endmodule
--- test/test.py
+++ test/test.py
@@ -1,40 +1,235 @@
 # SPDX-FileCopyrightText: © 2024 Tiny Tapeout
-# SPDX-License-Identifier: Apache-2.0
+# SPDX-License-Identifier: MIT
 
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
+import cocotb.result
+from cocotb.triggers import Timer, Edge, with_timeout
+import numpy as np
+
+BOARD_WIDTH = 8
+BOARD_HEIGHT = 8
 
 
-@cocotb.test()
-async def test_project(dut):
+@cocotb.test(timeout_time=100, timeout_unit='ms')
+async def test(dut):
     dut._log.info("Start")
 
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, units="us")
+    # Set the clock period to 24 MHz
+    clock = Clock(dut.clk, 41666, units="ps")
     cocotb.start_soon(clock.start())
 
-    # Reset
+    # UART signals
+    uart_rx = dut.ui_in[3]
+    uart_tx = dut.uo_out[4]
+
+    # GPIO config
+    do_gpio_config(dut)
+
+    # reset
+    await do_reset(dut)
+    assert uart_tx == 1
+
+    # send CR over UART to trigger init message
+    f = cocotb.start_soon(send_cmd(dut, uart_rx, 13))
+    init_str = await get_uart_str(dut, uart_tx)
+    await f
+
+    # check for correct init string
+    assert init_str == INIT_STRING
+    dut._log.info("Received correct init string")
+
+    # send '0' and receive board update
+    await Timer(0.7, units="ms")
+    f = cocotb.start_soon(send_cmd(dut, uart_rx, ord('0')))
+    board_state_str = await get_uart_str(dut, uart_tx)
+    await f
+
+    # check board update
+    board_state = parse_board_state(board_state_str)
+    dut._log.info("RANDOMIZED")
+    dut._log.info(board_state)
+
+    # advance step by step for 5 steps and check correct state evolution
+    for count in range(5):
+        board_state_correct = next_board_state(board_state)
+
+        # send '1' and receive board update
+        await Timer(0.25, units="ms")
+        f = cocotb.start_soon(send_cmd(dut, uart_rx, ord('1')))
+        await Edge(dut.uo_out)
+        board_state_str = await get_uart_str(dut, uart_tx)
+        await f
+
+        board_state = parse_board_state(board_state_str)
+        dut._log.info(f"STEP #{count+1}")
+        dut._log.info(board_state)
+
+        assert np.array_equal(board_state, board_state_correct)
+
+    dut._log.info("All good!")
+
+@cocotb.test(timeout_time=100, timeout_unit='ms')
+async def test2(dut):
+    dut._log.info("Start")
+
+    # Set the clock period to 24 MHz
+    clock = Clock(dut.clk, 41666, units="ps")
+    cocotb.start_soon(clock.start())
+
+    # UART signals
+    uart_rx = dut.ui_in[3]
+    uart_tx = dut.uo_out[4]
+
+    # GPIO config
+    do_gpio_config(dut)
+
+    # reset
+    await do_reset(dut)
+    assert uart_tx == 1
+
+    # send CR over UART to trigger init message
+    f = cocotb.start_soon(send_cmd(dut, uart_rx, 13))
+    init_str = await get_uart_str(dut, uart_tx)
+    await f
+
+    # check for correct init string
+    assert init_str == INIT_STRING
+    dut._log.info("Received correct init string")
+
+    # compute correct 1-step evolution of the initial state
+    board_state = np.array([1 if c == "1" else 0 for c in "1100000111010000100010100100011001111110100110011101110110010111"[::-1]], dtype=int).reshape(8,8)
+    board_state_correct = next_board_state(board_state)
+
+    # send '1' and receive board update
+    await Timer(0.25, units="ms")
+    f = cocotb.start_soon(send_cmd(dut, uart_rx, ord('1')))
+    await Edge(dut.uo_out)
+    board_state_str = await get_uart_str(dut, uart_tx)
+    await f
+
+    board_state = parse_board_state(board_state_str)
+    dut._log.info(board_state)
+
+    assert np.array_equal(board_state, board_state_correct)
+
+    dut._log.info("All good!")
+
+
+# HELPER FUNCTIONS
+
+async def send_cmd(dut, uart_rx, cmd=13):
+    dut._log.info("Sending: 0x%02X" % cmd)
+    await do_tx(uart_rx, 115200, cmd)
+
+async def do_reset(dut):
     dut._log.info("Reset")
     dut.ena.value = 1
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
     dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
+    await Timer(1, units="us")
     dut.rst_n.value = 1
+    await Timer(5, units="us")
 
-    dut._log.info("Test project behavior")
+def do_gpio_config(dut):
+    dut._log.info("GPIO config")
+    # GPIO IN
+    dut.ui_in.value = 0
+    # set RX high
+    dut.ui_in[3].value = 1
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    # GPIO IN/OUT
+    dut.uio_in.value = 0
 
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
+async def do_tx(uart_rx, baud, data):
+    # prepare random test data
+    TEST_BITS_LSB = [(data >> s) & 1 for s in range(8)]
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    # send start bit (0), 8 data bits, stop bit (1)
+    for tx_bit in [0] + TEST_BITS_LSB + [1]:
+        uart_rx.value = tx_bit
+        await Timer(int(1.0 / baud * 1e12), units="ps")
 
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+async def do_rx(dut, uart_tx, baud, timeout_us=0):
+    if timeout_us > 0:
+        count = 0
+        did_timeout = False
+        while uart_tx.value == 1 and count < timeout_us:
+            count += 1
+            try:
+                await with_timeout(Edge(dut.uo_out), 1, 'us')
+                did_timeout = False
+            except cocotb.result.SimTimeoutError:
+                did_timeout = True
+
+        if did_timeout:
+            return None
+    else:
+        while uart_tx.value == 1:
+            await Edge(dut.uo_out)
+    
+    assert uart_tx.value == 0
+
+    # wait 1/2 bit
+    await Timer(int(0.5 / baud * 1e12), units="ps")
+    # check start bit
+    assert uart_tx.value == 0
+
+    # 8 data bits
+    data = 0
+    for i in range(8):
+        await Timer(int(1.0 / baud * 1e12), units="ps")
+        data |= (1 << i) if uart_tx.value == 1 else 0
+
+    # check stop bit
+    await Timer(int(1.0 / baud * 1e12), units="ps")
+    assert uart_tx.value == 1
+
+    return data
+
+async def get_uart_str(dut, uart_tx):
+    blist = []
+
+    while True:
+        rx_byte = await do_rx(dut, uart_tx, 115200, timeout_us=100)
+        if rx_byte == None:
+            break
+        dut._log.info("Received [%d]: 0x%02X" % (len(blist), rx_byte))
+        blist.append(rx_byte)
+
+    return bytes(blist).decode()
+
+INIT_STRING = "\x1bc" + "\x1b[92m" + "Hello!\r\nspace: start/stop\r\n0: randomize\r\n1: step\r\n"
+ESC_STRING = "\x1b[;H"
+
+def parse_board_state(state_string):
+    assert state_string[:4] == ESC_STRING
+    assert len(state_string) == 4 + BOARD_HEIGHT * BOARD_WIDTH + (BOARD_HEIGHT-1)*2
+    assert len( set(state_string[4:]).difference(['O',' ','\r','\n']) ) == 0
+    rows = state_string[4:].split("\r\n")
+    assert len(rows) == BOARD_HEIGHT
+    assert [len(r) for r in rows] == BOARD_HEIGHT * [BOARD_WIDTH]
+    return np.array( [[1 if c=='O' else 0 for c in r] for r in rows] )
+
+def num_neighbors(state, i, j):
+    i_above = (i-1) if (i > 0) else (BOARD_HEIGHT-1)
+    i_below = (i+1) if (i < BOARD_HEIGHT-1) else (0)
+    j_left = (j-1) if (j > 0) else (BOARD_WIDTH-1)
+    j_right = (j+1) if (j < BOARD_WIDTH-1) else (0)
+
+    neighs = 0
+    neighs += state[i_above, j_left] + state[i_above, j] + state[i_above, j_right]
+    neighs += state[i, j_left] + state[i, j_right]
+    neighs += state[i_below, j_left] + state[i_below, j] + state[i_below, j_right]
+
+    return neighs
+
+# Conway's Game of Life update step
+def next_board_state(state):
+    next_state = np.zeros((BOARD_HEIGHT, BOARD_WIDTH), dtype=int)
+    for i in range(BOARD_HEIGHT):
+        for j in range(BOARD_WIDTH):
+            n = num_neighbors(state, i, j)
+            if (state[i,j] != 0 and (n == 2)) or (n == 3):
+                next_state[i,j] = 1
+    return next_state
+
